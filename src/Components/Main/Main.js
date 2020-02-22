7fae8bb5557806559bce0d8ffbb37a569aec88f3
Paul Hendrickson <PaulRobHendrickson@gmail.com>
2020-02-21 23:11:08 -0600

    removed bootstrap

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -5,28 +5,18 @@ import profilePic from "../../Media/prof-pic.jpeg";
 function Main() {
   return (
     <div className="Main">
-      <div className="row Stretchy">
-        <div className="col-8">
-          <div className="MainContainer">
-            <h1 style={{ marginBottom: "24px" }}>Hello, my name is Paul</h1>
-            <h3>
-              I am a full-stack web developer well-versed in HTML, CSS and
-              javascript and am comfortable handling both front and back end
-              systems. I am also a music arranger, singer, songwriter, pianist,
-              and TTRPG player. I bring an equal degree of passion, creativity,
-              and enthusiasm to all of my projects. I excel most with process
-              management, innovation management, and dealing with ambiguity.
-            </h3>
-          </div>
-        </div>
-        <div className="col-4" style={{ display: "flex" }}>
-          <img
-            className="ProfilePic"
-            alt="Paul Hendrickson"
-            src={profilePic}
-          ></img>
-        </div>
+      <div className="MainContainer Stretchy">
+        <h1 style={{ marginBottom: "24px" }}>Hello, my name is Paul</h1>
+        <h3>
+          I am a full-stack web developer well-versed in HTML, CSS and
+          javascript and am comfortable handling both front and back end
+          systems. I am also a music arranger, singer, songwriter, pianist, and
+          TTRPG player. I bring an equal degree of passion, creativity, and
+          enthusiasm to all of my projects. I excel most with process
+          management, innovation management, and dealing with ambiguity.
+        </h3>
       </div>
+      <img className="ProfilePic" alt="Paul Hendrickson" src={profilePic}></img>
     </div>
   );
 }
--- src/Components/PortfolioCard/PortfolioCard.js
+++ src/Components/PortfolioCard/PortfolioCard.js
@@ -5,7 +5,6 @@ function PortfolioCard(props) {
   return (
     <div className="PortfolioCard">
       <h1>{props.props.name}</h1>
-      Click here for:{" "}
       <div className="MyRow">
         <span>
           <a style={{ textDecoration: "underline" }} href={props.props.URL}>
@@ -27,7 +26,6 @@ function PortfolioCard(props) {
       </div>
       <p style={{ margin: "12px" }}>{props.props.description}</p>
       <img
-        className="MyImages"
         alt="Paul Hendrickson"
         src={process.env.PUBLIC_URL + props.props.image}
       />
