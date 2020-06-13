f7d3237622daf5b62fbcb9df093e5a3b224edd26
Paul Hendrickson <paulrobhendrickson@gmail.com>
2020-06-13 01:16:59 -0500

    Update Main.js

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -7,14 +7,6 @@ function Main() {
     <div className="Main">
       <div className="MainContainer Stretchy">
         <h1 style={{ marginBottom: "24px" }}>Hello, my name is Paul</h1>
-        <h3>
-          I am a full-stack web developer well-versed in HTML, CSS and
-          JavaScript and am comfortable handling both front and back end
-          systems. I am also a music arranger, singer, songwriter, pianist, and
-          TTRPG player. I bring an equal degree of passion, creativity, and
-          enthusiasm to all of my projects. I excel most with process
-          management, innovation management, and dealing with ambiguity.
-        </h3>
       </div>
       <img className="ProfilePic" alt="Paul Hendrickson" src={profilePic}></img>
     </div>
