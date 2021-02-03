414290a17f9a0cd229e0ecb27061f007714d7c34
Paul Hendrickson <PaulRobHendrickson@gmail.com>
2021-02-03 16:38:57 -0600

    added yarn and animation

--- src/Pages/PortfolioPage.js
+++ src/Pages/PortfolioPage.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Tween } from "react-gsap";
+
 import "./Pages.css";
 import { PortfolioInfo } from "../Media/PortfolioInfo";
 import PortfolioCard from "../Components/PortfolioCard/PortfolioCard";
@@ -6,11 +8,15 @@ import PortfolioCard from "../Components/PortfolioCard/PortfolioCard";
 function MainPage() {
   return (
     <div className="ContainMe">
-      {/* <h1 style={{ textAlign: "center", marginBottom: "48px" }}>Portfolio</h1> */}
+      <h1 style={{ textAlign: "center", marginBottom: "48px" }}>Portfolio</h1>
       <div className="PortfolioSection">
-        {PortfolioInfo.map((data) => {
-          return <PortfolioCard key={data.name} props={data} />;
-        })}
+        <Tween from={{ opacity: 0 }} stagger={0.5} duration={3}>
+          {PortfolioInfo.map((data) => (
+            <div key={data.name + "wrapper"}>
+              <PortfolioCard key={data.name} props={data} />
+            </div>
+          ))}
+        </Tween>
       </div>
     </div>
   );
