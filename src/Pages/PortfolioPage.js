a6d6ec3f0f531faf8dcc290f9a04ed8140145a5c
Paul Hendrickson <PaulRobHendrickson@gmail.com>
2021-03-31 14:44:19 -0500

    changed to filter by OR

--- src/Pages/PortfolioPage.js
+++ src/Pages/PortfolioPage.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import { Tween } from "react-gsap";
 
 import "./PortfolioPage.scss";
 import PortfolioInfo from "../Media/PortfolioInfo.json";
@@ -28,11 +27,17 @@ function MainPage() {
   const [cards, setCards] = useState(allCards);
 
   useEffect(() => {
-    setCards(
-      allCards.filter((card) =>
-        filters.every((filter) => card.technologies.includes(filter))
-      )
-    );
+    if (filters.length === 1) {
+      setCards(allCards);
+    } else {
+      setCards(
+        allCards.filter((card) =>
+          filters.some(
+            (filter) => card.technologies.includes(filter) && filter !== ""
+          )
+        )
+      );
+    }
   }, [filters]);
 
   return (
@@ -43,7 +48,10 @@ function MainPage() {
 
           <div className="MyRow" style={{ justifyContent: "space-between" }}>
             <div style={{ width: "102px" }}></div>
-            <h4>Filter by technology:</h4>
+            <h4>
+              Filter out projects that don't include any of the following
+              technologies:
+            </h4>
             <ul className="SkillsList">
               <li>
                 <button
@@ -66,27 +74,18 @@ function MainPage() {
           />
         </div>
         <div className="PortfolioSection">
-          {cards.length === 0 ? (
-            <h2>
-              I haven't made any projects that do ALL that in one! Maybe try
-              removing a filter or two.
-            </h2>
-          ) : (
-            cards.map((card) => (
-              <Tween from={{ opacity: 0 }} duration={2}>
-                <div key={card.name + "wrapper"}>
-                  <PortfolioCard
-                    redirect={redirect}
-                    setRedirect={setRedirect}
-                    setFilters={setFilters}
-                    filters={filters}
-                    key={card.name}
-                    cardInfo={card}
-                  />
-                </div>
-              </Tween>
-            ))
-          )}
+          {cards.map((card) => (
+            <div key={card.name + "wrapper"}>
+              <PortfolioCard
+                redirect={redirect}
+                setRedirect={setRedirect}
+                setFilters={setFilters}
+                filters={filters}
+                key={card.name}
+                cardInfo={card}
+              />
+            </div>
+          ))}
         </div>
       </div>
     </>
